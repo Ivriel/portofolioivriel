7cced13c3f735696e6617b576d65e4a82580e93a
Ivriel Gunawan <gunawanivriel@gmail.com>
2025-02-10 21:08:05 +0700

    Update project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import { forwardRef } from "react"
 function About(props, ref2) {
   return (
     <div ref={ref2} className="flex text-white mt-[50px] h-96 bg-red-500" id="about">
-    <h1>About Me</h1>
+    <h1>Masih Progress Pak Belum selesai</h1>
     <div>
       
     </div>
