61d36decb0e6248cb40ab47fefaf558f3e56ed92
Ivriel Gunawan <gunawanivriel@gmail.com>
2025-02-11 15:17:21 +0700

    update Contact component text to 'Kontak'

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import { forwardRef } from "react"
 
 function Contact(props,ref4) {
   return (
-    <div ref={ref4} className="text-white h-96 bg-yellow-400">Contact</div>
+    <div ref={ref4} className="text-white h-96 bg-yellow-400">Kontak</div>
   )
 }
 
