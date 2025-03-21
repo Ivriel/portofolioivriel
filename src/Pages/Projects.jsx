d515a48503d6a3e9db5d267c8f11910c2de11fd1
Ivriel Gunawan <gunawanivriel@gmail.com>
2025-03-21 10:25:01 +0700

    Add Shankara Paperstraw project with updated description and image

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -8,6 +8,7 @@ import POLINEMA from "../assets/projects/POLINEMAPICTURE.png"
 import LIGASMK from "../assets/projects/LIGASMKPICTURE.png"
 import TYPEMASTER from "../assets/projects/TYPEMASTERPICTURE.png"
 import CODECOMMUNITY from "../assets/projects/CODECOMMUNITYPICTURE.png"
+import SHANKARA from "../assets/projects/SHANKARAPICTURE.png"
 import { useState } from "react";
 import { motion } from "framer-motion";
 
@@ -85,10 +86,19 @@ function Projects() {
       link: "https://code-community-lime.vercel.app/",
       repository: "https://github.com/Ivriel/CODE-Community"
     }
+    ,
+    {
+      title: "Shankara Paperstraw",
+      description: "Company landing page website to promote their product and their company.", // Update description
+      tech: ["Next JS","Tailwind CSS"],
+      image: SHANKARA, // You'll need to import this image
+      link: "https://www.shankarapaperstraw.com/",
+      repository: "https://github.com/Ivriel/shankara-paperstraw" // Update with actual repository
+}
   ];
 
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = currentPage === 1 ? 6 : 3;
+  const itemsPerPage = currentPage === 1 ? 6 : 6;
   const startIndex = currentPage === 1 ? 0 : 6; // Page 1 starts at index 0, Page 2 starts at index 6
   const paginatedItems = projectsFile.slice(startIndex, startIndex +itemsPerPage);
 
@@ -110,7 +120,7 @@ function Projects() {
           </div>
         </div>
         <h2 className="text-[24px] font-bold mt-4">{project.title}</h2>
-        <p className="text-[18px] mt-4">{project.description}</p>
+        <p className="text-[18px] mt-4 ">{project.description}</p>
         <div className="flex gap-4 mt-4">
           {project.tech.map((tech, index) => (<ChipText key={index} text={tech} />))}
         </div>
